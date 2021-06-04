8a81bb5d5056665b952be2f96459ad3d6a6ca903
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-04 19:09:21 -0300

    - Adição de comentarios para entendimento do codigo

--- Aula02HowTheCppLinkerWorks/Log.h
+++ Aula02HowTheCppLinkerWorks/Log.h
@@ -12,5 +12,12 @@
 
 //#endif /* Log_h */
 
+//Repare que na linha abaixo estamos fazendo algo nao recomendado(que devia ser escrito em "log.hpp"
+// e não nesse arquivo atual "Log.h"), mas que funcionará, pois o compilador ao chamar o metodo
+// Logr(const char*) no arquivo main.cpp ele primeiro procura se em algum arquivo .hpp existe essa
+// declaracao de metodo, e existindo ele vai buscar a implementacao desse metodo em algum arquivo
+// *.cpp(que nesse caso foi o log.cpp) que foi transformado em binario(nesse caso log.o).
+// Resumindo: O compilador não se importa se a declaracao do metodo esta em um arquivo header com nome
+// diferente do arquivo cpp que contem a implementacao do metodo, nesse caso os arquivos "log.cpp" e "Log.h"
 void Logr(const char* mensagem);
 
